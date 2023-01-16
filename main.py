270c8723c8435388b4030ea882d2103becbc5854
tchow2020 <lgmds2017@outlook.com>
2023-01-16 17:10:03 -0300

    Feito

---
+++ main.py
@@ -0,0 +1,45 @@
+"""Calculadora!"""
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+),(-),(/),(*): ')
+    
+    numeros_validos = None
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+    
+    if numeros_validos is None:
+        print('os números não são válidos')
+        continue
+
+    operadores_permitidos = '(+), (-), (/), (*)'
+
+    if operador not in operadores_permitidos:
+        print('operador inválido.')
+        continue 
+        
+    if len(operador) > 1:
+        print('Digite apenas 1 operador por vez!')
+        continue     
+    
+    print('realizando sua conta, confira abaixo!')
+    if operador == '+':
+        print(num_1_float + num_2_float)
+    elif operador == '-':
+        print(num_1_float - num_2_float)
+    elif operador == '*':
+        print(num_1_float * num_2_float)
+    elif operador == '/':
+        print(num_1_float / num_2_float)
+    else:
+        print('Sá porra tá errada')
+    
+    sair = input('Quer sair? ').lower().startswith('s')
+    
+    if sair is True:
+        break
